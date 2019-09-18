810b1dd16718a962689466822decb3937e014dd0
rhhsu <50847645+rhhsu@users.noreply.github.com>
2019-09-18 12:54:25 -0400

    Spacing

--- test_calculate.py
+++ test_calculate.py
@@ -9,6 +9,7 @@ def test_add(a, b, expected):
     result = add(a, b)
     assert result == pytest.approx(expected)
 
+
 @pytest.mark.parametrize("c, d, expected2", ([
     (4, 2, 2), (5, 1, 4), (8, 2, 6)]))
 def test_sub(c, d, expected2):
